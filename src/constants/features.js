4b270f80508eb1483b2a1306c97716370e990c26
ayodeji <ayodejiakintobi1@gmail.com>
2023-10-17 17:18:32 +0100

    UPDATE; STYLING ADDED

---
+++ src/constants/features.js
@@ -0,0 +1,30 @@
+import { images } from './images';
+
+
+export const featuresData = [
+    {
+        img: 'images.one',
+        title: 'REQUEST A RIDE',
+        description:
+            'Have to reach the office or going for shopping? Just put your current location and destination and search a ride that suits you.',
+    },
+    {
+        img: 'images.Two',
+        title: 'INSTANT NOTIFICATIONS',
+        description:
+            'Get instant notification for your rides and be in contact with fellow riders all the time.',
+    },
+    {
+        img: 'images.Three',
+        title: 'POST A RIDE',
+        description: 
+           'Going somewhere but hate to Just post your ride details and destination and search and publish it'
+    },
+    {
+        img: 'images.Four',
+        title: 'CASHLESS PAYMENT',
+        description: 
+        'Payment made easy by using your own Wallet - no more cash to carry'
+    },
+] 
+
